01dac58d8424620bf8aedd8b5676b039c9482edf
Yannick LA ROSA <larosa@antadis.com>
2023-02-02 14:45:29 +0100

    finission add user commande

--- config/definitions.php
+++ config/definitions.php
@@ -18,31 +18,4 @@ use Sheepdev\Repository\UserRepository;
 ;
 
 return [
-    'AppLogger' => function(ContainerInterface $c) {
-        return new AppLogger();
-    },
-    'PasswordCryptService' => function(ContainerInterface $c) {
-        return new PasswordCryptService();
-    },
-    'UserNormalizer' => function(ContainerInterface $c) {
-        return new UserNormalizer();
-    },
-    'UserRepository' => function(ContainerInterface $c) {
-        return new UserRepository(
-            $c->get('UserNormalizer'),
-            $c->get('AppLogger')
-        );
-    },
-    'UserController' => function (ContainerInterface $c) {
-        return new UserController($c->get('UserRepository'));
-    },
-    'AddUserAction' => function(ContainerInterface $c) {
-        return new AddUserAction(
-            $c->get('UserRepository'),
-            $c->get('PasswordCryptService')
-        );
-    },
-    'AddUserCommand' => function(ContainerInterface $c) {
-        return new AddUserCommand($c->get('UserRepository'));
-    },
 ];
--- src/Command/AddUserCommand.php
+++ src/Command/AddUserCommand.php
@@ -4,9 +4,10 @@ namespace Sheepdev\Command;
 
 use Sheepdev\Actions\Users\AddUser\AddUserAction;
 use Sheepdev\Actions\Users\AddUser\AddUserRequest;
-use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Command\Command;
 
 class AddUserCommand extends Command
 {
@@ -22,14 +23,50 @@ class AddUserCommand extends Command
         $this->action = $action;
     }
 
+    protected function configure(): void
+    {
+        $this
+            ->addOption('firstname', 'f', InputOption::VALUE_REQUIRED)
+            ->addOption('lastname', 'l', InputOption::VALUE_REQUIRED)
+            ->addOption('email', 'e', InputOption::VALUE_REQUIRED)
+            ->addOption('password', 'p', InputOption::VALUE_REQUIRED)
+            ->addOption('roles', 'r', InputOption::VALUE_OPTIONAL);
+    }
+
     protected function execute(InputInterface $input, OutputInterface $output)
     {
+        if ($input->getOption('firstname') == '') {
+            $output->writeln('<error>Firstname must not be empty</error>');
+            return self::FAILURE;
+        }
+        if ($input->getOption('lastname') == '') {
+            $output->writeln('<error>Lastname must not be empty</error>');
+            return self::FAILURE;
+        }
+        if ($input->getOption('email') == '') {
+            $output->writeln('<error>Email must not be empty</error>');
+            return self::FAILURE;
+        }
+        if ($input->getOption('password') == '') {
+            $output->writeln('<error>Password must not be empty</error>');
+            return self::FAILURE;
+        }
+
+        $roles = [];
+        if ($input->hasArgument('roles')) {
+            $roles = $input->getOption('roles');
+            $roles = explode(',', $roles);
+            $roles = array_map(function($item) {
+                return trim($item);
+            }, $roles);
+        }
+
         $data = new AddUserRequest();
-        $data->firstname = 'Jenna';
-        $data->lastname = 'BISHOP';
-        $data->email = 'jenna.bishop@example.com';
-        $data->password = 'sheepdev';
-        $data->roles = ['author','reviewer'];
+        $data->firstname = $input->getOption('firstname');
+        $data->lastname = $input->getOption('lastname');
+        $data->email = $input->getOption('email');
+        $data->password = $input->getOption('password');
+        $data->roles = $roles;
         $response = $this->action->execute($data);
 
         if (!empty($response->errors)) {
